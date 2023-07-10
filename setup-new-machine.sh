70e9d3eb18f9f9fc0c9ca161c667502aee7f6675
ricardog <rick.godinez44@gmail.com>
2023-07-10 12:34:49 -0700

    updated script

--- setup-new-machine.sh
+++ setup-new-machine.sh
@@ -1,6 +1,40 @@
-#!/bin/zsh
-
+# !/bin/zsh
+# https://brew.sh/
 # install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL 
+https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# software installations
+brew install docker
+brew install python
 
+# app installations
+brew install --cask google-chrome-dev
+brew install --cask fire-developer-edition
+brew install --cask brave-browser-dev
+
+brew install --cask todoist
+brew install --cask notion
+brew install --cask alfred
+brew install --cask google-drive
+brew install --cask spotify
+brew install --cask discord
+
+brew install --cask ccleaner
+brew install --cask malwarebytes
+
+brew install --cask microsoft-teams
+brew install --cask microsoft-office
+brew install --cask onedrive
+brew install --cask adobe-creative-cloud
+
+brew install --cask iterm2
+brew install --cask powershell
+brew install --cask virtualbox
+brew install --cask visual-studio
+brew install --cask visual-studio-code
+brew install --cask raspberry-pi-imager
+brew install --cask ltspice
+brew install --cask keka
+
+brew update && brew upgrade
